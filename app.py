d857bdf3a8ac8d3abd7bcd8013de0c820ea4d034
llluo <1979707287@qq.com>
2018-03-23 11:07:44 +0800

    update the template

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ def download_file(filename):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=2333, debug=True)
+    app.run(host='0.0.0.0', port=2335, debug=True)
     # init_all_table()
 
     # for i in range(2000):
